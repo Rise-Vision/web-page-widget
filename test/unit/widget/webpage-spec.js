cd5d440a23b41139dd959719ab592e52f5b19d80
Stuart Lees <stuartlees@gmail.com>
2015-02-26 16:49:36 -0500

    Removing retrieval of companyId from Viewer
    - No longer obtaining companyId via rpc, companyId for `rise-storage` purposes is now provided via Settings
    - Widget source updated to remove all use of companyId from Viewer
    - bower updated to use latest releases of several dependencies and remove unnecessary resolutions for `angular-translate`
    - Version bump to `0.1.2`

--- test/unit/widget/webpage-spec.js
+++ test/unit/widget/webpage-spec.js
@@ -4,7 +4,6 @@ describe("Web Page Widget - RiseVision.WebPage", function() {
   it("should exist", function() {
     expect(RiseVision.WebPage).to.exist;
     expect(RiseVision.WebPage).to.be.an("object");
-    expect(RiseVision.WebPage.setCompanyId).to.be.a("function");
     expect(RiseVision.WebPage.setAdditionalParams).to.be.a("function");
     expect(RiseVision.WebPage.pause).to.be.a("function");
     expect(RiseVision.WebPage.play).to.be.a("function");
