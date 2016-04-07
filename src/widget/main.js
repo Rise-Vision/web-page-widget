c8a6f3d4b7382ce1492adb690da30bd8a09ffa0b
Stuart Lees <stuartlees@gmail.com>
2016-04-07 11:57:54 -0400

    Adding support for logging to BQ [stage-3]
    - Updated dependency for widget-common
    - Importing use of config and logger modules from widget-common
    - Added support for logging to widget implementation

--- src/widget/main.js
+++ src/widget/main.js
@@ -1,15 +1,51 @@
 /* global RiseVision, gadgets */
 
-(function (window, gadgets) {
+(function (window, document, gadgets) {
   "use strict";
 
-  var prefs = new gadgets.Prefs(),
-    id = prefs.getString("id");
+  var id = new gadgets.Prefs().getString("id");
 
   // Disable context menu (right click menu)
   window.oncontextmenu = function () {
     return false;
   };
+
+  document.body.onmousedown = function() {
+    return false;
+  };
+
+  function configure(names, values) {
+    var additionalParams,
+      companyId = "",
+      displayId = "";
+
+    if (Array.isArray(names) && names.length > 0 && Array.isArray(values) && values.length > 0) {
+      // company id
+      if (names[0] === "companyId") {
+        companyId = values[0];
+      }
+
+      // display id
+      if (names[1] === "displayId") {
+        if (values[1]) {
+          displayId = values[1];
+        }
+        else {
+          displayId = "preview";
+        }
+      }
+
+      // provide LoggerUtils the ids to use
+      RiseVision.Common.LoggerUtils.setIds(companyId, displayId);
+
+      // additional params
+      if (names[2] === "additionalParams") {
+        additionalParams = JSON.parse(values[2]);
+
+        RiseVision.WebPage.setAdditionalParams(additionalParams);
+      }
+    }
+  }
 
   function play() {
     RiseVision.WebPage.play();
@@ -23,14 +59,6 @@
     RiseVision.WebPage.stop();
   }
 
-  function additionalParams(names, values) {
-    if (Array.isArray(names) && names.length > 0 && names[0] === "additionalParams") {
-      if (Array.isArray(values) && values.length > 0) {
-        RiseVision.WebPage.setAdditionalParams(JSON.parse(values[0]));
-      }
-    }
-  }
-
   function polymerReady() {
     window.removeEventListener("WebComponentsReady", polymerReady);
 
@@ -38,14 +66,13 @@
       gadgets.rpc.register("rscmd_play_" + id, play);
       gadgets.rpc.register("rscmd_pause_" + id, pause);
       gadgets.rpc.register("rscmd_stop_" + id, stop);
-
-      gadgets.rpc.register("rsparam_set_" + id, additionalParams);
-      gadgets.rpc.call("", "rsparam_get", null, id, ["additionalParams"]);
+      gadgets.rpc.register("rsparam_set_" + id, configure);
+      gadgets.rpc.call("", "rsparam_get", null, id, ["companyId", "displayId", "additionalParams"]);
     }
   }
 
   window.addEventListener("WebComponentsReady", polymerReady);
 
-})(window, gadgets);
+})(window, document, gadgets);
 
 
