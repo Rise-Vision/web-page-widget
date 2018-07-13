294a9906851e4b3ee7788940d68837b1a889f8b2
santiagonoguez <santiago@nul-unu.com>
2018-07-13 12:07:16 -0500

    test case where query string already exists

--- test/unit/widget/cache-buster-spec.js
+++ test/unit/widget/cache-buster-spec.js
@@ -14,4 +14,11 @@ describe( "withCacheBusterAppended()", function() {
     expect( page.withCacheBusterAppended( url ) ).to.match( regex );
   } );
 
+  it( "should append a cache buster parameter to an URL that already has a query string", function() {
+    var url = "http://localhost:8080/?param=1&param=2",
+      regex = /http:[/][/]localhost:8080[/][?]param=1&param=2&__cachebuster__=\d+/;
+
+    expect( page.withCacheBusterAppended( url ) ).to.match( regex );
+  } );
+
 } );
