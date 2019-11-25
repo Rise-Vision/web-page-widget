111a727c63ab11488e41226dbd9a51bb57999a31
santiagonoguez <santiago@nul-unu.com>
2019-11-25 17:54:20 -0600

    add timeout

--- test/e2e/settings.js
+++ test/e2e/settings.js
@@ -66,12 +66,16 @@
         expect( element( by.css( "input[type='radio'][value='page']" ) ).isSelected() ).to.eventually.be.true;
       } );
 
-      it( "Should apply ''Never Refresh' for Refresh'", function() {
-        // element.all( by.css( "select[name='refresh'] option" ) ).then( function( elements ) {
-        //   expect( elements.length ).to.equal( 5 );
-        // } );
+      it( "Should apply ''Never Refresh' for Refresh'", function( done ) {
+        setTimeout( function() {
+          element.all( by.css( "select[name='refresh'] option" ) ).then( function( elements ) {
+            console.log( '----------------------------' + elements.length );
+            // expect( elements.length ).to.equal( 5 );
+          } );
 
-        expect( element( by.id( "refresh" ) ).getAttribute( "value" ) ).to.eventually.equal( "0" );
+          expect( element( by.css( "select[name='refresh']" ) ).getAttribute( "value" ) ).to.eventually.equal( "0" );
+          done();
+        }, 100 );
       } );
 
       it( "Should select 'Unload Web Page'", function() {
